2d04af49e0eb979b98ffbf9e18683b49b27d8c7d
itsjaysuthar <jaysuthar0603@gmail.com>
2023-02-05 12:51:44 -0600

    the web app is ready

--- app.py
+++ app.py
@@ -23,5 +23,18 @@ def predict_api():
     #print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    scalar = pickle.load(open('scaling.pkl','rb'))
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="the house price prediction is {}".format(output))
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
